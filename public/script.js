22f07f27ccf69751ebb82ba349ab212cff525a17
Oluwafemi Timilehin <timilehinoluwafemi566@gmail.com>
2025-09-03 15:31:40 +0100

    Updated script.js and other changes

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ document.getElementById('contactForm').addEventListener('submit', async function
     const response = await fetch('/contact', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json' 
       },
       body: JSON.stringify({ name, email, message })
     });
